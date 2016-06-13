a5048719630d85301911e3ef31e976bc154f7d6a
shparikh <shparikh@us.ibm.com>
2016-06-13 15:21:49 -0400

    second

--- test_chaincode.go
+++ test_chaincode.go
@@ -16,7 +16,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
         return nil, errors.New("Incorrect number of arguments. Expecting 1")
     }
 
-    err := stub.PutState("hello_world", []byte(args[0]))
+    err := stub.PutState("hello_world", []byte(["Hi there"]))
     //stub.PutState("a",[]byte(args[0]))
     if err != nil {
         return nil, err
